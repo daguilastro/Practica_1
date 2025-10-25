491bbf8ad7d658876a6cb716b04ec1a95a2e87a5
daguilastro <daaguilarc@unal.edu.co>
2025-10-25 07:21:59 -0500

    Version 2 de la practica 1 falta reescribir build_sorted_index.cpp

--- func_client.hpp
+++ func_client.hpp
@@ -7,30 +7,7 @@
 #include <cstring>
 #include <vector>
 
-
 using namespace std;
-
-struct RowInfo {
-    // Resultado
-    int resultado;              // 1 = ganó, 0 = perdió
-    double duracion;            // minutos
-    
-    // Equipos
-    vector<string> equipo_azul;
-    vector<string> equipo_rojo;
-    
-    // Stats del jugador
-    int kills;
-    int deaths;
-    int assists;
-    int cs;
-    int gold_earned;
-    int damage_to_champions;
-    int damage_taken;
-    int vision_score;
-    int level;
-    double kda;
-};
 
 void send_request(const string& name);
 
